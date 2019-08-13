ea375135fe0bceda24f5a217707c859b3ea1d944
Amar2784 <46690746+Amar2784@users.noreply.github.com>
2019-08-13 17:47:38 -0400

    Add files via upload

---
+++ Amarjeet_Singh_Using_Tables/Amarjeet_Singh_Using_Tables/Amarjeet_TableCell.swift
@@ -0,0 +1,26 @@
+//
+//  Amarjeet_TableCell.swift
+//  Amarjeet_Singh_Using_Tables
+//
+//  Created by Amarjeet Singh on 2019-08-13.
+//  Copyright © 2019 Amarjeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class Amarjeet_TableCell: UITableViewCell {
+
+    @IBOutlet weak var oneLabel: UILabel!
+    var count = 0
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Amarjeet_Singh_Using_Tables/Amarjeet_Singh_Using_Tables/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amarjeet_Singh_Using_Tables
+//
+//  Created by Amarjeet Singh on 2019-08-13.
+//  Copyright © 2019 Amarjeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
